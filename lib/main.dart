9590f213a78f3392c105496234607712634b9b98
grig <grig_p@mail.ru>
2024-08-14 15:07:35 +0300

    Запрет поворота телефона

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:bgs_control/repositories/bgs_connect/ble_service.dart';
 import 'package:bgs_control/repositories/bgs_list/bgs_list.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter/services.dart';
 
 import 'bgs_app.dart';
 
@@ -11,5 +12,11 @@ void main() {
   GetIt.I.registerLazySingleton<BgsConnect>(() => BgsConnect());
   GetIt.I.registerLazySingleton<BgsList>(() => BgsList());
 
-  runApp(const BgsApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(const BgsApp());
+  });
+
+//  runApp(const BgsApp());
 }
