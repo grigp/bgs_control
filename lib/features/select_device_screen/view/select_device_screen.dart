01183e1a463c3c93a400fb997860d512e08e9b02
grig <grig_p@mail.ru>
2024-08-20 11:02:02 +0300

    Добавлен экран select_methodic_screen

--- lib/features/select_device_screen/view/select_device_screen.dart
+++ lib/features/select_device_screen/view/select_device_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:bgs_control/features/direct_control_screen/view/direct_control_screen.dart';
 import 'package:bgs_control/features/select_device_screen/features/add_new_device_bottom_sheet/add_new_device_bottom_sheet.dart';
 import 'package:bgs_control/features/select_device_screen/widgets/missing_result_tile.dart';
 import 'package:bgs_control/features/uikit/styles.dart';
@@ -12,6 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get_it/get_it.dart';
 
+import '../../select_methodic_screen/view/select_methodic_screen.dart';
 import '../widgets/scan_result_tile.dart';
 
 class SelectDeviceScreen extends StatefulWidget {
@@ -229,12 +229,19 @@ class _SelectDeviceScreenState extends State<SelectDeviceScreen> {
   }
 
   void onSelectPressed(BluetoothDevice device) {
+    // MaterialPageRoute route = MaterialPageRoute(
+    //   builder: (context) => DirectControlScreen(
+    //     title: 'Direct',
+    //     device: device,
+    //   ),
+    //   settings: const RouteSettings(name: '/direct_control'),
+    // );
     MaterialPageRoute route = MaterialPageRoute(
-      builder: (context) => DirectControlScreen(
-        title: 'Direct',
+      builder: (context) => SelectMethodicScreen(
+        title: 'Выбор методики',
         device: device,
       ),
-      settings: const RouteSettings(name: '/direct_control'),
+      settings: const RouteSettings(name: '/select_method'),
     );
     Navigator.of(context).push(route);
 
