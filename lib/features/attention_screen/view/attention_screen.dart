5aeb2e06007b76875f6b0e8f2c0d3d4e48e183b3
grig <grig_p@mail.ru>
2024-12-16 11:10:05 +0300

    Новая версия противопоказаний

--- lib/features/attention_screen/view/attention_screen.dart
+++ lib/features/attention_screen/view/attention_screen.dart
@@ -23,7 +23,7 @@ class _AttentionScreenState extends State<AttentionScreen> {
         padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            const SizedBox(height: 40),
+            const SizedBox(height: 20),
             Center(
               child: Image.asset('images/attention.png'),
             ),
@@ -35,31 +35,30 @@ class _AttentionScreenState extends State<AttentionScreen> {
               'имеются противопоказания',
               style: theme.textTheme.titleLarge,
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             Text(
               'Элетростимуляция никогда не должна применяться:',
               style: theme.textTheme.titleMedium,
             ),
-            const SizedBox(height: 10),
             Expanded(
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Text(
-                    '- При беременности;\n'
-                        '- У людей, страдающих от грыжи или выпячивания органов;\n'
-                        '- К голове;\n'
-                        '- К людям, страдающим эпилепсией;\n'
-                        '- В местах травматических повреждений нервов, мышц и сухожилий;\n'
-                        '- В острый период пишемических поражений тканей мозга и сердца;\n'
-                        '- В местах заболевания кожи.',
-                    style: theme.textTheme.titleMedium,
-                  ),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Text(
+                  '- Для детей;\n'
+                  '- При беременности;\n'
+                  '- К голове, в области сердца, в области крупных сосудов, при варикозном расширении вен, тромбозе, тромбофлебите; в области открытых и закрытых кровотечений, в местах травматических повреждений костей, нервов, мышц, сухожилий и мягких тканей, в области грыж и выпячивании тканей; в области поражений и заболеваний кожи (в том числе в области невусов (родимых пятен);\n'
+                  '- Лицам с неустойчивой психикой;\n'
+                  '- Лицам с эпилепсией, кардиостимуляторами (искусственными водителями ритмов сердца), в острый период ишемических и геморрагических поражений тканей мозга и сердца, онкологическими заболеваниями;\n'
+                  '- При появлении аллергических реакций;\n'
+                  '- При управлении транспортными средствами, механизмами, электротехническим оборудованием;\n'
+                  '- при нарушении целостности изделия.\n\n'
+                  'После процедуры возможно кратковременное покраснение в области воздействия. Не допускайте болевых и дискомфортных ощущений. Не допускайте использование электрода с сухими токопроводящими площадками.',
+                  style: theme.textTheme.titleSmall,
                 ),
-
+              ),
             ),
             Center(
-              child:
-              TexelButton.accent(
+              child: TexelButton.accent(
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
