dd88185b02958ac57032b9ded2e378c9ef258b07
grig <grig_p@mail.ru>
2025-03-24 15:44:54 +0300

    Новые пороги мощности после модернизации

--- lib/utils/base_defines.dart
+++ lib/utils/base_defines.dart
@@ -1,7 +1,7 @@
 /// Уровень заряда, при котором показывается сообщение о необходимости зарядить аккумулятор
-const double chargeAlarmBoundLevel = 20;
+const double chargeAlarmBoundLevel = 15;
 /// Уровень заряда, при котором невозможно запускать программы
-const double chargeBreakBoundLevel = 0;
+const double chargeBreakBoundLevel = 5;
 
 /// Время максимального воздействия в режиме Direct Control при отсутствии нажатия на кнопки
 const double maxTimeDirectControlMode = 3600;
