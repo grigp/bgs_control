850f36aff0d3bc27c9ebad5787df937713c531e7
grig <grig_p@mail.ru>
2025-12-22 18:07:44 +0300

    Окно результатов. Испрввлено положение названия методики

--- lib/features/result_screen/view/result_screen.dart
+++ lib/features/result_screen/view/result_screen.dart
@@ -38,132 +38,140 @@ class _ResultScreenState extends State<ResultScreen> {
       },
       child: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              const SizedBox(height: 60),
-              Text(
-                'Сеанс завершен',
-                style: theme.textTheme.titleLarge,
-              ),
-              Text(
-                widget.driver.program.title,
-                style: theme.textTheme.titleMedium,
-              ),
-              const SizedBox(height: 60),
-              Row(
-                children: [
-                  const Spacer(),
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: 120,
-                        width: 120,
-                        child: CustomPaint(
-                          painter: CircularValueDiag.text(
-                            getTimeBySecCount(widget.driver.programTime()),
-                            'мин:сек',
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 150,
-                        child: Text(
-                          'Продолжительность, мин:сек',
-                          style: theme.textTheme.titleSmall,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  const SizedBox(width: 30),
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: 120,
-                        width: 120,
-                        child: CustomPaint(
-                          painter: CircularValueDiag(
-                            value: widget.driver.maxPower().toInt(),
-                            min: 0,
-                            max: 125,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          'Максимальный уровень воздействия',
-                          style: theme.textTheme.titleSmall,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: 120,
-                        width: 120,
-                        child: CustomPaint(
-                          painter: CircularValueDiag(
-                            value: widget.driver.averagePower().toInt(),
-                            min: 0,
-                            max: 125,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 120,
-                        child: Text(
-                          'Средний уровень воздействия',
-                          style: theme.textTheme.titleSmall,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 30),
-                ],
-              ),
-              const Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(
-                  right: 14,
-                  left: 14,
-                  bottom: 14,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 2,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 60),
+                Text(
+                  'Сеанс завершен',
+                  style: theme.textTheme.titleLarge,
                 ),
-                child: TexelButton.accent(
-                  onPressed: () {
-                    Navigator.of(context).popUntil(
-                      ModalRoute.withName('/select_method'),
-                    );
-                  },
-                  text: 'Выйти',
+                const SizedBox(height: 10),
+                Text(
+                  widget.driver.program.title,
+                  style: theme.textTheme.titleMedium,
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              // TexelButton.accent(
-              //   onPressed: () {
-              //     Navigator.of(context)
-              //         .popUntil(ModalRoute.withName('/select_method'));
-              //   },
-              //   text: 'Сохранить и выйти',
-              // ),
-              // const SizedBox(height: 10),
-              // TexelButton.secondary(
-              //   onPressed: () {
-              //     Navigator.of(context)
-              //         .popUntil(ModalRoute.withName('/select_method'));
-              //   },
-              //   text: 'Выйти без сохранения',
-              // ),
-            ],
+                const SizedBox(height: 60),
+                Row(
+                  children: [
+                    const Spacer(),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 120,
+                          width: 120,
+                          child: CustomPaint(
+                            painter: CircularValueDiag.text(
+                              getTimeBySecCount(widget.driver.programTime()),
+                              'мин:сек',
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 150,
+                          child: Text(
+                            'Продолжительность, мин:сек',
+                            style: theme.textTheme.titleSmall,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    const SizedBox(width: 30),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 120,
+                          width: 120,
+                          child: CustomPaint(
+                            painter: CircularValueDiag(
+                              value: widget.driver.maxPower().toInt(),
+                              min: 0,
+                              max: 125,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            'Максимальный уровень воздействия',
+                            style: theme.textTheme.titleSmall,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Column(
+                      children: [
+                        SizedBox(
+                          height: 120,
+                          width: 120,
+                          child: CustomPaint(
+                            painter: CircularValueDiag(
+                              value: widget.driver.averagePower().toInt(),
+                              min: 0,
+                              max: 125,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 120,
+                          child: Text(
+                            'Средний уровень воздействия',
+                            style: theme.textTheme.titleSmall,
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 30),
+                  ],
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 14,
+                    left: 14,
+                    bottom: 14,
+                  ),
+                  child: TexelButton.accent(
+                    onPressed: () {
+                      Navigator.of(context).popUntil(
+                        ModalRoute.withName('/select_method'),
+                      );
+                    },
+                    text: 'Выйти',
+                  ),
+                ),
+                // TexelButton.accent(
+                //   onPressed: () {
+                //     Navigator.of(context)
+                //         .popUntil(ModalRoute.withName('/select_method'));
+                //   },
+                //   text: 'Сохранить и выйти',
+                // ),
+                // const SizedBox(height: 10),
+                // TexelButton.secondary(
+                //   onPressed: () {
+                //     Navigator.of(context)
+                //         .popUntil(ModalRoute.withName('/select_method'));
+                //   },
+                //   text: 'Выйти без сохранения',
+                // ),
+              ],
+            ),
           ),
         ),
       ),
